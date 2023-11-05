c0206c7f3662640f16944e355f0e205e3b6b3f67
Maxim Tsukanov <m.tsukanov@shipnext.com>
2023-11-05 14:43:56 +0200

    add task

---
+++ lesson2/homewotk2.js
@@ -0,0 +1,30 @@
+/* Additional materials:
+https://www.w3schools.com/js/js_type_conversion.asp
+https://javascript.info/type-conversions
+https://www.programiz.com/javascript/type-conversion
+https://www.geeksforgeeks.org/javascript-type-conversion/
+https://medium.com/@13__TOMMY/type-conversion-javascript-basics-a6c55e71cada
+https://dev.to/alexanie_/javascript-type-conversions-1p3g
+*/
+
+//Вам необхідно вказати, яким буде результат операції та описати крок за кроком у вигляді коментарів, як відбувається перетворення в кожному прикладі:
+
+console.log('number' + 3 + 3)  
+console.log(null + 3)
+console.log(5 && "qwerty")
+console.log(+'40' + +'2' + "hillel")
+console.log('10' - 5 === 6)
+console.log(true + false)
+console.log('4px' - 3)
+console.log('4' - 3)
+console.log('6' + 3 ** 0)
+console.log(12 / '6')
+console.log('10' + (5 === 6))
+console.log(null == '')
+console.log(3 ** (9 / 3))
+console.log(!!'false' == !!'true')
+console.log(0 || '0' && 1)
+console.log((+null == false) < 1)
+console.log(false && true || true)
+console.log(false && (false || true))
+console.log((+null == false) < 1 ** 5)
